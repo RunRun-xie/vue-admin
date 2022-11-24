4996a274ce45d56e9050810714da952bfac205f3
runTu <17891981464@163.com>
2022-11-24 15:55:58 +0800

    1

---
+++ src/views/placeholder-page/tips-components/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <button
+    @click="
+      Message.success({
+        message: 'this is a message',
+      })
+    "
+  >
+    Success
+  </button>
+  <button
+    @click="
+      Message.warning({
+        warning: 'this is a Warning',
+      })
+    "
+  >
+    Warning
+  </button>
+  <button
+    @click="
+      Message({
+        type: types.MESSAGE,
+        message: 'this is a message',
+      })
+    "
+  >
+    Message
+  </button>
+
+  <button
+    @click="
+      Message({
+        time: 1000,
+        type: types.ERROR,
+        message: 'this is a error',
+      })
+    "
+  >
+    Error
+  </button>
+  <test-message :duration="5000" :type="'warning'" message="this is message"></test-message>
+</template>
+
+<script setup lang="ts">
+import Message, { types } from '@/components/Message'
+// import testMessage from '@/components/Message/Message.vue'
+</script>
+
+<style scoped></style>
