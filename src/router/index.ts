3acf9eb539a234192829041089a5cd25ddb4df79
runTu <17891981464@163.com>
2023-04-10 11:55:49 +0800

    增添nprogress加载进度条样式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { staticRouter } from "./modules/staticRouter";
+import { staticRouter, errorRouter } from "./modules/staticRouter";
 
 const router = createRouter({
 	history: createWebHistory(),
-	routes: [...staticRouter],
+	routes: [...staticRouter, ...errorRouter],
 	//strict： 路由 /users 将匹配 /users、/users/、甚至 /Users/
 	strict: false // 适用于所有路由
 });
--- src/router/modules/staticRouter.ts
+++ src/router/modules/staticRouter.ts
@@ -1,8 +1,45 @@
 import type { RouteRecordRaw } from "vue-router";
 
+// 静态路由地址
 export const staticRouter: RouteRecordRaw[] = [
 	{
 		path: "/",
-		redirect: "/login/index"
+		redirect: "/login"
+	},
+	{
+		path: "/login",
+		name: "login",
+		component: () => import("@/views/login/index.vue"),
+		meta: {
+			title: "登录"
+		}
+	}
+];
+
+// 错误页面地址
+export const errorRouter: RouteRecordRaw[] = [
+	{
+		path: "/403",
+		name: "403",
+		component: () => import("@/components/ErrorMessage/403.vue"),
+		meta: {
+			title: "403页面"
+		}
+	},
+	{
+		path: "/404",
+		name: "404",
+		component: () => import("@/components/ErrorMessage/404.vue"),
+		meta: {
+			title: "404页面"
+		}
+	},
+	{
+		path: "/500",
+		name: "500",
+		component: () => import("@/components/ErrorMessage/500.vue"),
+		meta: {
+			title: "500页面"
+		}
 	}
 ];
